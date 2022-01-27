4b73ec7c7189128b4fa5ec342514bab87680f9ec
pankajchauhan11288 <60954593+pankajchauhan11288@users.noreply.github.com>
2022-01-27 23:31:28 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import ContactUs from "./Components/Pages/ContactUs";
 import Seervices from "./Components/Pages/Seervices";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Layout/Navbar";
+import PageNotFound from "./Components/Pages/PageNotFound";
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
-        <dir className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/ContactUs" element={<ContactUs />} />
-            <Route path="/Seervices" element={<Seervices />} />
-          </Routes>
-        </dir>
+        <div className="jumbotron-fluid">
+          <Navbar />
+          <dir className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/About" element={<About />} />
+              <Route path="/ContactUs" element={<ContactUs />} />
+              <Route path="/Seervices" element={<Seervices />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </dir>
+        </div>
       </BrowserRouter>
     </>
   );
