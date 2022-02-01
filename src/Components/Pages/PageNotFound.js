10d5221a200e88dd6fb6cc23985ef40fdadf61c4
pankajchauhan11288 <60954593+pankajchauhan11288@users.noreply.github.com>
2022-02-01 08:32:58 +0530

    new changes of link button

--- src/Components/Pages/PageNotFound.js
+++ src/Components/Pages/PageNotFound.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
 const PageNotFound = () => {
   return <div>
       <h1 className='display-1'>Page Not Found</h1>
+      <Link className="link" to="/">Go to Home</Link>
   </div>;
 };
 
